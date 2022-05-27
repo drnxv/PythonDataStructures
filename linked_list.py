5f57e3b657f449371b8dd7d40b718901fbe38e67
drnxv <87783549+drnxv@users.noreply.github.com>
2022-05-27 16:59:10 -0400

    modified all files to be formatted and accurate

--- linked_list.py
+++ linked_list.py
@@ -1,60 +1,60 @@
 # this is going to be a linked list implementation in python
 class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
+  def __init__(self, data=None):
+    self.data = data
+    self.next = None
 
 class List:
-    def __init__(self):
-        self.head = None
+  def __init__(self):
+    self.head = None
 
-    def add(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-        else:
-            curr = self.head
-            while curr.next:
-                curr = curr.next
-
-            curr.next = new_node
-
-    def remove_node(self, data):
+  def add(self, data):
+      new_node = Node(data)
+      if not self.head:
+        self.head = new_node
+      else:
         curr = self.head
-        prev = None
-
-        # special case if the head needs to be removed
-        if self.head.data == data:
-            self.head = self.head.next
-        else:
-            while curr:
-                if curr.data == data:
-                    prev.next = curr.next
-
-                prev = curr
-                curr = curr.next
-
-    # clear the linked list
-    def reset(self):
-        self.head = None
-
-    # to string method
-    def __str__(self):
-        res = ''
-        curr = self.head
-        while curr:
-            res += str(curr.data) + ' '
+        while curr.next:
             curr = curr.next
 
-        return res
+        curr.next = new_node
+
+  def remove_node(self, data):
+      curr = self.head
+      prev = None
+
+      # special case if the head needs to be removed
+      if self.head.data == data:
+        self.head = self.head.next
+      else:
+        while curr:
+          if curr.data == data:
+            prev.next = curr.next
+
+          prev = curr
+          curr = curr.next
+
+  # clear the linked list
+  def reset(self):
+    self.head = None
+
+  # to string method
+  def __str__(self):
+    res = ''
+    curr = self.head
+    while curr:
+        res += str(curr.data) + ' '
+        curr = curr.next
+
+    return res
 
 def main():
-    nums = List()
-    nums.add(3)
-    nums.add(2)
-    nums.add(1)
-    nums.remove_node(3)
-    print(nums)
+  nums = List()
+  nums.add(3)
+  nums.add(2)
+  nums.add(1)
+  nums.remove_node(3)
+  print(nums)
 
 if __name__ == '__main__':
-    main()
+  main()
--- stack_linked.py
+++ stack_linked.py
@@ -1,54 +1,60 @@
 # this is an implementation of a stack using a linked list
 class Node:
-    def __init__(self, data=None):
-        self.next = None
-        self.data = data
+  def __init__(self, data=None):
+    self.next = None
+    self.data = data
 
 
 class Stack:
-    def __init__(self):
-        self.head = None
+  def __init__(self):
+    self.head = None
 
-    def push_node(self, data):
-        n = Node(data)
-        if not self.head:
-            self.head = n
-        else:
-            curr = self.head
-            while curr.next:
-                curr = curr.next
+  def push_node(self, data):
+    n = Node(data)
+    if not self.head:
+      self.head = n
+    else:
+      curr = self.head
+      while curr.next:
+        curr = curr.next
 
-            curr.next = n
+      curr.next = n
 
-    def pop_node(self):
-        res = self.head.data
-        self.head = self.head.next
-        return res
+  def pop_node(self):
+    curr = self.head
+    while curr.next.next:
+      curr = curr.next
 
-    def is_empty(self) -> bool:
-        return self.head is None
+    last = curr.next
+    res = last.data
+    curr.next = None 
+    last = None
+    return res
 
-    def __str__(self):
-        curr = self.head
-        res = ''
-        while curr:
-            res += str(curr.data) + ' '
-            curr = curr.next
+  def is_empty(self) -> bool:
+    return self.head is None
 
-        return res
+  def __str__(self):
+    curr = self.head
+    res = ''
+    while curr:
+      res += str(curr.data) + ' '
+      curr = curr.next
+
+    return res
 
 
 def main():
-    stack = Stack()
-    stack.push_node(12)
-    stack.push_node(45)
-    stack.push_node(78)
-    stack.push_node(11)
-    stack.push_node(90)
-    print(stack)
-    print(stack.pop_node())
-    print(stack)
+  stack = Stack()
+  stack.push_node(12)
+  stack.push_node(45)
+  stack.push_node(78)
+  stack.push_node(11)
+  stack.push_node(90)
+  print(stack)
+  print(stack.pop_node())
+  print(stack)
 
 
 if __name__ == '__main__':
-    main()
+  main()
